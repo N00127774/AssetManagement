0425152a4ad07cfc524426a014268b257a94efab
Baabatunde Adegboye <Adegboyetunde24@yahoo.ie>
2015-02-26 19:19:39 +0000

    added the branch class to this CA

---
+++ src/com/software/app/Branch.java
@@ -0,0 +1,82 @@
+package com.software.app;
+
+public class Branch {
+ private int branchID;
+ private String address;
+ private String number;
+ private String openingHours;
+ private String managerName;
+ 
+ 
+ public Branch(int bId, String a, String n, String o, String mn){
+ 
+  this.branchID=bId;
+  this.address=a;
+  this.number=n;
+  this.openingHours=o;
+  this.managerName=mn;
+
+}
+ 
+ public Branch(String a,String n, String o, String mn){
+     this(-1,a,n,o,mn);
+ }
+ 
+ /* get methods*/
+ public int getBranchID (){
+     return branchID;
+ }
+ 
+ public String getAddress(){
+     return address;
+ }
+ 
+ public String getNumber(){
+    return number; 
+ }
+ 
+ public String OpeningHours(){
+     return openingHours;
+ }
+ 
+ public String ManagerName(){
+     return managerName;
+ }
+ 
+ 
+ /* set Methods*/
+ public void setBranchID(int branchID){
+     this.branchID= branchID;
+ }
+ 
+ public void setAddress(String address){
+     this.address=address;
+ }        
+ 
+ public void setNumber(String number){
+     this.number=number;
+ }
+ 
+ public void setOpeningHours(String openingHours){
+     this.openingHours=openingHours;
+ }
+ 
+ public void setManagerName(String managerName){
+     this.managerName=managerName;
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+}
